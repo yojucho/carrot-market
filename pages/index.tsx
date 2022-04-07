12739aad338a14e4613494f92711b9cac9f79ff9
cho youngjune <yojucho.jp@gmail.com>
2022-04-07 19:09:20 +0900

    second commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-yellow-500'>
+      <h1 className='text-red-500'>Hello carrot-market</h1>
+    </div>
+  );
+};
+
+export default Home;
